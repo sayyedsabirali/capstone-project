93ba18c057b5784c77d3cec42a6906181eada0d7
SayyedSabir <sabirali969091@gmail.com>
2025-08-28 05:14:29 +0530

    completed

--- data_download.py
+++ data_download.py
@@ -1,7 +1,1 @@
-import pandas as pd
 
-url = "https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv"
-df = pd.read_csv(url)
-print("DataFrame loaded. Here's a preview:")
-print(df.head())
-df.to_csv("data.csv", index=False)
